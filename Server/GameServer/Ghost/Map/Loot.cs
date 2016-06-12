d0868d0e527bd6da567a17a16dee23c435549026
o黯淡o <york8817612@hotmail.com>
2016-06-12 13:49:57 +0800

    補上傳Loot.cs

---
+++ Server/GameServer/Ghost/Map/Loot.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.Ghost
+{
+    public class Loot
+    {
+        public int ID { get; set; }
+        public int MobID { get; set; }
+        public int ItemID { get; set; }
+        public int MinimumQuantity { get; private set; }
+        public int MaximumQuantity { get; private set; }
+        public int QuestID { get; private set; }
+        public int Chance { get; private set; }
+
+        public Loot(dynamic datum, bool fromMob = true)
+        {
+            this.MobID = datum.mobID;
+            this.ItemID = datum.itemID;
+
+            if (fromMob)
+            {
+                this.MinimumQuantity = datum.min_quantity;
+                this.MaximumQuantity = datum.max_quantity;
+            }
+
+            this.QuestID = datum.questID;
+            this.Chance = datum.chance;
+        }
+    }
+}
