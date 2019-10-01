0ad3ab14f54e7c186e30d8cb9f7310ba52edc3bc
PLEUMKUNG <sopanus2014@gmail.com>
2019-10-01 21:21:59 +0700

    translate ServerState

--- Server/LoginServer/Net/ServerState.cs
+++ Server/LoginServer/Net/ServerState.cs
@@ -2,17 +2,17 @@
 {
     public class ServerState
     {
-        /* state
-         * 07 - 因使用Bug進行遊戲，帳號已凍結
-         * 08 - 因不正當賺取金錢，帳號已凍結
-         * 09 - 因口出穢言，帳號已凍結
-         * 10 - 因洗頻，帳號已凍結
-         * 11 - 帳號暫時被凍結
-         * 12 - 帳號已凍結
-         * 13 - 帳號錯誤，請重新輸入
-         * 14 - 您所輸入的密碼錯誤
-         * 29 - 因偵測到不當的遊戲進行方式，1小時內將無法進行遊戲
-         */
+    /* state
+          * 07 - Account is frozen due to gameplay using Bug
+          * 08 - Account has been frozen due to improper money earning
+          * 09 - Account has been frozen due to rumors
+          * 10 - The account has been frozen due to frequency washing
+          * 11 - Account is temporarily frozen
+          * 12 - Account is frozen
+          * 13 - Account number is incorrect, please re-enter
+          * 14 - The password you entered is incorrect
+          * 29 - The game will not be played within 1 hour due to the detection of improper gameplay.
+          */
         public enum LoginState
         {
             OK = 0,
@@ -28,11 +28,11 @@
             HACK_LOCK = 29
         }
 
-        /* state
-         * 28 = 此為有年齡限制的頻道，請使用其他頻道
-         * 04 = 此ID已連線，請稍後再試
-         * else = 網路狀態錯誤
-         */
+       /* state
+          * 28 = This is an age-restricted channel, please use another channel
+          * 04 = This ID is connected, please try again later
+          * else = network status error
+          */
         public enum ChannelState
         {
             OK = 0,
